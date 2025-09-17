6100dc618ac14cd72bbee34e4b33d5d7fddf9460
PS Solutions Tecnology <92067743+PauloTIgit@users.noreply.github.com>
2025-09-16 21:06:07 -0300

    feat: melhora da funcao que converte seu htm em pdf generate-pdf

--- app/main.py
+++ app/main.py
@@ -1,20 +1,28 @@
-from fastapi import FastAPI, UploadFile, Form
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import JSONResponse
 from app.pdf_generator import html_to_pdf
-from io import BytesIO
+import os
+import uuid
 
-app = FastAPI(title="HTML to PDF API")
+app = FastAPI()
 
-@app.post("/generate-pdf/")
-async def generate_pdf(html: str = Form(...), file: str = 'saida'):
-    """
-    Recebe HTML via form-data e retorna PDF
-    """
-    pdf_bytes = html_to_pdf(html)
-    return StreamingResponse(BytesIO(pdf_bytes), media_type="application/pdf", headers={
-        'Content-Disposition': 'attachment; filename='+file+'.pdf'
+GENERATED_DIR = "generated"
+os.makedirs(GENERATED_DIR, exist_ok=True)
+
+@app.post("/generate_pdf")
+async def generate_pdf(html: str = Form(...)):
+    file_id = str(uuid.uuid4())
+    file_path = f"{GENERATED_DIR}/{file_id}.pdf"
+    html_to_pdf(html, file_path)
+    return JSONResponse(content={
+        "download_url": f"http://44.222.249.128:8082/download/{file_id}.pdf"
     })
 
-@app.get("/")
-async def root():
-    return {"message": "HTML to PDF API funcionando!"}
+from fastapi.responses import FileResponse
+
+@app.get("/download/{file_name}")
+async def download_pdf(file_name: str):
+    file_path = os.path.join(GENERATED_DIR, file_name)
+    if os.path.exists(file_path):
+        return FileResponse(file_path, media_type="application/pdf", filename=file_name)
+    return JSONResponse(content={"error": "Arquivo não encontrado"}, status_code=404)

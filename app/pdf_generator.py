f43f68f5989e61c98ced65b6d9fa476996592fd5
PS Solutions Tecnology <92067743+PauloTIgit@users.noreply.github.com>
2025-09-18 16:22:52 -0300

    ajustes de css para o pdf

--- app/pdf_generator.py
+++ app/pdf_generator.py
@@ -2,5 +2,5 @@
 from weasyprint import HTML, CSS
 
 def html_to_pdf(html: str) -> bytes:
-    css = CSS(string='@page { size: A4; margin: 20mm; }')
+    css = CSS(string='@page { size: A4; margin: 0mm; }')
     return HTML(string=html).write_pdf(stylesheets=[css])
